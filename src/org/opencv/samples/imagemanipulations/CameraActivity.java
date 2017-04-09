36eb9ac2f2bf6f8f8ca1192c52ecc7ed90895850
Oboridav <oboridav@gmail.com>
2017-04-09 23:14:40 +0200

    Removed unused imports
    2

--- src/org/opencv/samples/imagemanipulations/CameraActivity.java
+++ src/org/opencv/samples/imagemanipulations/CameraActivity.java
@@ -1,22 +1,11 @@
 package org.opencv.samples.imagemanipulations;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfFloat;
-import org.opencv.core.MatOfInt;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.MatOfPoint2f;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
